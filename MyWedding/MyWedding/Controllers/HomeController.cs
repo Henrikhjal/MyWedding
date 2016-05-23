02e0eec63bea342583fd5f2ab903174ae797eb7b
Henrik <hjalmarsson.henrik@live.com>
2016-05-23 18:22:44 +0200

    Added language etc

--- MyWedding/MyWedding/Controllers/HomeController.cs
+++ MyWedding/MyWedding/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace MyWedding.Controllers
     {
         public ActionResult Index()
         {
+            Utility.Utility.setLanguage(Request);
             return View();
         }
 
@@ -33,6 +34,55 @@ namespace MyWedding.Controllers
             Utility.Utility.setLanguage(Request);
             Utility.Utility.WriteLog("Home-Contacts", (string)Session["UserId"]);
             return View();
+        }
+
+        public ActionResult Info()
+        {
+            Utility.Utility.setLanguage(Request);
+            Utility.Utility.WriteLog("Home-Info", (string)Session["UserId"]);
+            return View();
+        }
+
+        public ActionResult ChangeLanguageSwe()
+        {
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = "sv-SE";
+                cookie.Expires = DateTime.Now.AddMonths(3);
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                cookie.Value = "sv-SE";
+                cookie.Expires = DateTime.Now.AddMonths(3);
+                Response.SetCookie(cookie);
+            }
+
+            Utility.Utility.WriteLog("Home-ChangeLanguageSwe", (string)Session["UserId"]);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult ChangeLanguageUK()
+        {
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie == null)
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = "en-GB";
+                cookie.Expires = DateTime.Now.AddMonths(3);
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                cookie.Value = "en-GB";
+                cookie.Expires = DateTime.Now.AddMonths(3);
+                Response.SetCookie(cookie);
+            }
+
+            Utility.Utility.WriteLog("Home-ChangeLanguageUK", (string)Session["UserId"]);
+            return RedirectToAction("Index", "Home");
         }
     }
 }
