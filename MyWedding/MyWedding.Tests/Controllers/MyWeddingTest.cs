f0a3b0033b4646c77328a59ef38fc84cec88543d
Henrik <hjalmarsson.henrik@live.com>
2016-06-03 14:01:23 +0200

    small fixes

---
+++ MyWedding/MyWedding.Tests/Controllers/MyWeddingTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyWedding;
+using MyWedding.Controllers;
+using MyWedding.Repository;
+using MyWedding.Models;
+
+namespace MyWedding.Tests.Controllers
+{
+    [TestClass]
+    public class MyWeddingTest
+    {
+        // Unit tests will be created.
+        public void CanReserveWishListItem()
+        {
+            // arrange
+
+            // act
+
+            // assert
+        }
+    }
+}
+
