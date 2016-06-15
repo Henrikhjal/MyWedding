ba6dafc148447e4b303eb525f0d88bdd9aadbed6
Henrik Hjalmarsson <hjalmarsson.henrik@live.com>
2016-06-15 17:47:59 +0200

    Some work with unit tests.

--- MyWedding/MyWedding.Tests/Controllers/MyWeddingTest.cs
+++ MyWedding/MyWedding.Tests/Controllers/MyWeddingTest.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MyWedding;
 using MyWedding.Controllers;
 using MyWedding.Repository;
 using MyWedding.Models;
 using Moq;
+using System.Web;
 
 namespace MyWedding.Tests.Controllers
 {
@@ -40,6 +38,25 @@ namespace MyWedding.Tests.Controllers
             Assert.AreEqual(MessageArray[1].MessageText, "ett till meddelande");
         }
 
+
+        [TestMethod]
+        public void ReturnsWishlistView()
+        {
+            // arrange
+            Mock<IMyWeddingRepository> mock = new Mock<IMyWeddingRepository>();
+
+            mock.Setup(m => m.WishlistItems).Returns(new WishlistItem[]{
+                new WishlistItem{ Id=1, Name= "Item1", Details = "Item1 details", Price  = 100, Quantity=3, Reserved=1 },
+                new WishlistItem{ Id=2, Name= "Item2", Details = "Item2 details", Price  = 200, Quantity=4, Reserved=0 },
+                new WishlistItem{ Id=3,  Name= "Item3", Details = "Item3 details", Price  = 300, Quantity=2, Reserved=2 }
+                });
+
+            
+            //WishlistController controller = new WishlistController(mock.Object);
+            //var result = controller.Wishlist() as ViewResult;
+            //Assert.AreEqual("fooview", result.ViewName);
+        }
+
         [TestMethod]
         public void Can_Reserve_Item_In_Wishlist()
         {
@@ -54,6 +71,8 @@ namespace MyWedding.Tests.Controllers
 
 
             WishlistController controller = new WishlistController(mock.Object);
+
+
 
             // act
 
--- MyWedding/MyWedding/Controllers/WishlistController.cs
+++ MyWedding/MyWedding/Controllers/WishlistController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using MyWedding.Models;
 using MyWedding.Repository;
 using System.Net;
+
 
 namespace MyWedding.Controllers
 {
